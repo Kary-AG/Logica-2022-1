f97d7d8d1492875abbd359fb4cc20fcecbf9520a
Kary-AG <kary_agarcia@ciencias.uanm.mx>
2021-03-04 22:25:53 -0600

    mod

---
+++ Notas/Laboratorio/sesion1.hs
@@ -0,0 +1,13 @@
+Haskell
+
+https://www.haskell.org/
+
+  http://learnyouahaskell.com/chapters
+  http://zvon.org/other/haskell/Outputglobal/index.html
+  https://hoogle.haskell.org/
+
+- Funcional
+- estaticamente tipado  -> tipo definidos
+- inferencia de tipos
+
+Trabajar en haskell -> Intérprete GHCI HUGS

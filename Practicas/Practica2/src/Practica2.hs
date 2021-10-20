f5fcc075f28daa393400f40919e8e9aefd0c5896
Kary-AG <kary_agarcia@ciencias.unam.mx>
2021-10-19 21:38:55 -0500

    distr

--- Practicas/Practica2/src/Practica2.hs
+++ Practicas/Practica2/src/Practica2.hs
@@ -10,21 +10,34 @@
 module Practica2 where
 
 import LProp
-
+import Data.List
 
 -- |1| Funcion fnn que recibe una formula φ y devuelve su forma normal negativa
 fnn :: Prop -> Prop
-fnn = error "Implementar"
+fnn  Top  = Top
+fnn Bot   = Bot
+fnn (P x) =  (P x)
+fnn (Neg (P x))    = (Neg (P x))
+fnn (Neg x)   = fnn$ neg$elimImp$elimEquiv (Neg x)
+fnn (Or x y)  = (Or (fnn$ elimImp$elimEquiv x) (fnn$ elimImp$elimEquiv y))
+fnn (And x y) = (And (fnn$ elimImp$elimEquiv x) (fnn$ elimImp$elimEquiv y))
+fnn (Impl x y) = fnn$ elimImp$elimEquiv (Impl x y)
+fnn (Syss x y) = fnn$ elimImp$elimEquiv(Syss x y)
 
 -- |2| Funcion distr la cual aplica adecuadamente las leyes distributivas a una formula proposicional
 distr :: Prop -> Prop
-distr = error "Implementar"
-
+distr (Or (And p q) r) = distr(And ( Or (distr p)  (distr r)) (Or (distr q) (distr r)))
+distr (Or r (And p q)) = distr(And (Or (distr r) (distr p)) (Or (distr r)  (distr q)))
+distr (And f g) = And (distr f)  (distr g)
+distr f = f
 -- |3| Funcion fnc que recibe una formula φ y devuelve su forma normal conjuntiva, es decir:
 --     Permite expresar cualquier formula proposicional como una conjunción de disyunciones.
 -- IMPORTANTE: Se puede suponer que la formula de entrada ya se encuentra en forma normal negativa
 fnc :: Prop -> Prop
-fnc = error "Implementar"
+fnc (P x)       = (P x)
+fnc (Neg (P x)) = (Neg (P x))
+fnc (And x y)   = (And (fnc x) (fnc y))
+fnc (Or x y)    = distr$ Or(fnc x) (fnc y)
 
 --Definimos un tipo de dato Literal como sinónimo de fórmula proposicional
 type Literal = Prop
@@ -34,20 +47,69 @@ type Clausula = [Literal]
 
 -- |4| Función ctolist recibe una proposicion y devuelve la forma clausular de la proposicion
 ctolist :: Prop -> Clausula
-ctolist = error "Implementar"
+ctolist (P x)       = [(P x)]
+ctolist (Neg (P x)) = [(Neg(P x))]
+ctolist (And x y)   = union (ctolist x)  (ctolist y)
+ctolist (Or x y)    = union (ctolist x)  (ctolist y)
+ctolist (Impl x y)   = [(Impl x y)]
+ctolist (Syss x y)  = [(Syss x y)]
 
 -- |5| Función fncC recibe una fórmula en forma normal conjuntiva y debe devolver su conversión en una lista de cláusulas.
 fncC :: Prop -> [Clausula]
-fncC = error "Implementar"
+fncC (And x y)  = ctolist x:[ctolist y]
+fncC (Or x y)   = ctolist x:[ctolist y]
+fncC (Impl x y) = [ctolist (Impl x y)]
+fncC (Syss x y) = [ctolist (Syss x y)]
+
 
 -- |6| Funcion fncConj recibe una proposicion y devuelve el conjunto de clausulas equivalentes a esa proposicion
 fncConj :: Prop -> [Clausula]
-fncConj = error "Implementar"
-
-
+fncConj x = fncC$fnc$fnn x
+--Falta acomodar listas c:
 {--
 PUNTOS EXTRA
 --}
 -- |1| Función argcorrecto que verifica si un argumento con premisas y una conclusión es lógicamente correcto
 argcorrecto :: [Prop] -> Prop -> Bool
-argcorrecto = error "Implementar"
+argcorrecto xs  c= tautologia(Impl (verCor xs) c)
+
+{--
+FUNCIONES AUX
+-}
+-- [1']Función que regresa una fórmula equivalente donde las negaciones solo se aplican a fórmulas atómicas.
+neg :: Prop -> Prop
+neg (Neg (P x))    = (Neg (P x))
+neg (Neg (Neg x))  = elimImp$elimEquiv x
+neg (Neg(And x y)) = (Or (neg (Neg (elimImp$elimEquiv x))) (neg (Neg (elimImp$elimEquiv y))))
+neg (Neg(Or x y))  = (And (neg (Neg (elimImp$elimEquiv x))) (neg (Neg (elimImp$elimEquiv y))))
+
+verCor:: [Prop] -> Prop
+verCor (x:xs) = (And x (verCor xs))
+{--
+EJEMPLOS
+-}
+
+fnn1 = fnn$ Syss (P 'p') (P 'q')
+-- Devuelve: (¬ 'p' v 'q') ∧ (¬ 'q' v 'p'))
+fnn2 = fnn$ Impl (Or (P 'p') (P 'q')) (P 'p')
+-- Devuelve: ((¬ 'p' ∧ ¬ 'q') v 'p')
+distr1 = distr (Or (And (P 'p') (P 'q')) (P 'r'))
+-- Devuelve: (('p' v 'r') ∧ ('q' v 'r'))
+cto1 = ctolist (Neg (P 'p'))
+-- Devuelve: [¬ 'p']
+cto2 = ctolist (Or (Or (Neg (P 'p')) (P 'r')) (Or (Neg (P 'p')) (P 'q')))
+-- Devuelve: [¬ 'p','r','q']
+cto3 = ctolist (Impl (P 'p') (P 'q'))
+-- Devuelve: [('p' > 'q')]
+fncC1 = fncC (And (P 'p') (Or (Neg (P 'q')) (P 'r')))
+-- Devuelve: [['p'],[¬ 'q','r']]
+fncC2 = fncC (And (Or (Neg (P 'p')) (P 'q')) (Or (Neg (P 'p')) (Neg (P 'r'))))
+-- Devuelve: [[¬ 'p','q'],[¬ 'p',¬ 'r']]
+fncC3 = fncC (Syss (P 'p') (P 'r'))
+-- Devuelve: [[('p' <> 'r')]]
+fncConj1 = fncConj (Neg (Syss (P 'p') (P 'q')))
+-- Devuelve: [['p','q'],['p',¬ 'p'],[¬ 'q','q'],[¬ 'q',¬ 'p']]
+fncConj2 = fncConj (Or(Or (P 'p') (P 'q')) (Or(Neg (P 'q')) (P 'r')))
+--Devuelve: ['p','q',¬ 'q','r']
+argcorrecto1 = argcorrecto [(Impl (Neg (P 'r')) (Neg (P 'i'))), (Or (P 'e') (Neg(Neg (P 'i')))), (Neg (P 'e'))] (P 'r')
+-- Devuelve: True
